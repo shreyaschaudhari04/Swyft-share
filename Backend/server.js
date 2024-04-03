625f109e17a20e39d97600e5a19c70e45f062514
shreyaschaudhari04 <shreyasrchaudhari@gmail.com>
2024-04-04 00:11:43 +0530

    modified server.js

--- Backend/server.js
+++ Backend/server.js
@@ -15,8 +15,8 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // Routes 
-app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
+app.use('/api/files', require('./Routes/files'));
+app.use('/files', require('./Routes/show'));
 app.use('/files/download', require('./Routes/download'));
 
 
